e8fd913b5a762766568e68f421049fb220cd4475
LohitNimbagal <67277960+LohitNimbagal@users.noreply.github.com>
2024-03-12 21:44:51 +0530

    added projects data

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,92 +3,58 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import React from 'react'
 import goshopping from '../assets/goshopping.jpeg'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+import { Link } from 'react-router-dom'
+import projectsdata from '../../projectsdata.json'
 
-const projects = [
-  {
-    title: "PreVue",
-    // screenshot: "/assets/prevue.png",
-    gitLink: "https://github.com/LohitNimbagal/PreVue",
-    liveLink: "https://prevuee.vercel.app/",
-  },
-  {
-    title: "Password Generator",
-    // screenshot: "/assets/password-generator.png",
-    gitLink: "https://github.com/LohitNimbagal/Password-Generator",
-    liveLink: "https://paasswordgenerator.netlify.app/",
-  },
-  // {
-  //   title: "To-Do List App",
-  //   // screenshot: "assets/to-do-app.png",
-  //   gitLink: "https://github.com/LohitNimbagal/To-Do-App",
-  //   liveLink: "https://minimal-taskmanager.netlify.app/",
-  // },
-  // {
-  //   title: "BMI Calculator",
-  //   // screenshot: "/assets/bmi-calculator.png",
-  //   gitLink: "https://github.com/LohitNimbagal/minimalBMIcalculator",
-  //   liveLink: "https://minimalbmicalcalculator.netlify.app/",
-  // }, {
-  //   title: "Weather Checker",
-  //   // screenshot: "/assets/weather-app.png",
-  //   gitLink: "https://github.com/LohitNimbagal/minimalistweatherwebapp",
-  //   liveLink: "https://minimalistweatherwebapp.netlify.app/",
-  // },
-  // {
-  //   title: "Calculator",
-  //   // screenshot: "/assets/calculator.png",
-  //   gitLink: "https://github.com/LohitNimbagal/minimalistcalculator",
-  //   liveLink: "https://minimalistcalculator.netlify.app/",
-  // },
-  // {
-  //   title: "Bubble Game",
-  //   // screenshot: "/assets/buble-game.png",
-  //   gitLink: "https://github.com/LohitNimbagal/Bubble-Game",
-  //   liveLink: "https://bbubblegame.netlify.app/",
-  // }
-]
 
 export default function Projects() {
   return (
-    <div className='w-full h-full flex justify-evenly flex-wrap gap-10'>
-      {projects.map((project) => (
-        <Card className='w-full bg-white bg-opacity-30 backdrop-blur-1 rounded-lg flex py-5'>
+    <div className='w-full h-full flex justify-evenly flex-wrap'>
+      {projectsdata.map((project) => (
+        <Card key={project.title} className='w-full bg-white bg-opacity-30 backdrop-blur-1 rounded-lg  py-5 my-5 flex flex-col items-center'>
 
-          <Carousel className="w-full max-w-3xl p-5 px-12">
-            <CarouselContent>
-              <CarouselItem><img src={goshopping} alt="" /></CarouselItem>
-              <CarouselItem><img src={goshopping} alt="" /></CarouselItem>
-              <CarouselItem><img src={goshopping} alt="" /></CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious className='left-2' />
-            <CarouselNext className='right-2' />
-          </Carousel>
+          <CardHeader className='text-center'>
+            <CardTitle className='text-2xl'>{project.title}</CardTitle>
+            <CardDescription className='flex gap-3'>
+              {project.stack.map((item) => (
+                <span key={item}>{item}</span>
+              ))}
+            </CardDescription>
+          </CardHeader>
 
-          <div>
-            <CardHeader>
-              <CardTitle>{project.title}</CardTitle>
-              <CardDescription>E-Commers</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <ul className='list-disc my-1 text-sm'>
-                <h4 className='font-semibold'>About: </h4>
-                <li className='ml-5'>JWT Authentication</li>
-                <li className='ml-5'>Location-based currency conversion.</li>
-                <li className='ml-5'>User Convenience: Seamless currency adaptation.</li>
-              </ul>
+          <section className='lg:flex items-center justify-center'>
+            <Carousel className="w-full max-w-3xl px-12">
+              <CarouselContent>
+                <CarouselItem><img src={goshopping} alt="" /></CarouselItem>
+                <CarouselItem><img src={goshopping} alt="" /></CarouselItem>
+                <CarouselItem><img src={goshopping} alt="" /></CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious className='left-2' />
+              <CarouselNext className='right-2' />
+            </Carousel>
 
-              <ul className='list-disc my-1 text-sm'>
-                <h4 className='font-semibold'>New:</h4>
-                <li className='ml-5'>JWT Authentication</li>
-                <li className='ml-5'>Location-based currency conversion.</li>
-                <li className='ml-5'>User Convenience: Seamless currency adaptation.</li>
-              </ul>
-            </CardContent>
-            <CardFooter className='flex items-center justify-evenly'>
-              <Button>Live Link</Button>
-              <Button>GitHub</Button>
-            </CardFooter>
-          </div>
+            <div className='w-96 h-full flex flex-col items-start justify-start'>
+              <CardContent>
+                <ul className='list-disk my-1 text-sm'>
+                  <h4 className='font-semibold'>Discription </h4>
+                  {project.discription.map((feature) => (
+                    <li key={feature}>{feature}</li>
+                  ))
+                  }
+                </ul>
+              </CardContent>
+
+              <CardFooter className='flex items-center justify-start gap-5'>
+                <Link to={project.liveLink} target='_blank'>
+                  <Button>Live Link</Button>
+                </Link>
+                <Link to={project.gitLink} target='_blank'>
+                  <Button>GitHub</Button>
+                </Link>
+              </CardFooter>
+            </div>
+
+          </section>
         </Card>
       ))
       }
